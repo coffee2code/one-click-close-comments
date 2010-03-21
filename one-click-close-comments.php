d7f9c72dc49dbfa14093eff40a59035fc8150314
Scott Reilly <scott@coffee2code.com>
2010-03-21 01:08:34 -0400

    Recommit lost code for v2.0:
    * Display commenting status even if JS is disabled
    * Render commenting status as a 'span' instead of an 'a' and use unobtrusive JS to make it clickable
    * Insert column into desired position using PHP instead of JS
    * Fix issue related to disappearance of button for a post after using Quick Edit
    * Fix issue of 'Allow Comments' checkbox in 'Quick Edit' getting out of sync with actual comment status
    * Allow filtering of character used as click link, via 'one-click-close-comments-click-char'
    * Move initialization of config array out of constructor and into new function load_config()
    * Create init() to handle calling load_textdomain() and load_config() (textdomain must be loaded before initializing config)
    * Add support for localization
    * Add PHPDoc documentation
    * Add .pot file
    * Note compatibility with WP 2.9+
    * Drop compatibility with versions of WP older than 2.8
    * Update documentation (descriptions, FAQs, etc) to reflect behavior changes
    * Update copyright date

--- one-click-close-comments.php
+++ one-click-close-comments.php
@@ -1,10 +1,16 @@
 <?php
+/**
+ * @package One_Click_Close_Comments
+ * @author Scott Reilly
+ * @version 2.0
+ */
 /*
 Plugin Name: One Click Close Comments
-Version: 1.1
+Version: 2.0
 Plugin URI: http://coffee2code.com/wp-plugins/one-click-close-comments
 Author: Scott Reilly
 Author URI: http://coffee2code.com
+Text Domain: one-click-close-comments
 Description: Conveniently close or open comments for a post or page with one click.
 
 From the admin listing of posts ('Edit Posts') and pages ('Edit Pages'), a user can close or open comments to any
@@ -16,7 +22,7 @@ page reload.
 
 This plugin will only function for administrative users in the admin who have JavaScript enabled.
 
-Compatible with WordPress 2.6+, 2.7+, 2.8+.
+Compatible with WordPress 2.8+, 2.9+.
 
 =>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
 =>> for more information and the latest updates
@@ -24,14 +30,14 @@ Compatible with WordPress 2.6+, 2.7+, 2.8+.
 Installation:
 
 1. Download the file http://www.coffee2code.com/wp-plugins/one-click-close-comments.zip and unzip it into your 
-/wp-content/plugins/ directory.
+/wp-content/plugins/ directory (or install via the built-in WordPress plugin installer).
 2. Activate the plugin through the 'Plugins' admin menu in WordPress
 3. When on the 'Edit Posts' or 'Edit Pages' admin pages, click the indicator to toggle the comment status for a post, as necessary
 
 */
 
 /*
-Copyright (c) 2009 by Scott Reilly (aka coffee2code)
+Copyright (c) 2009-2010 by Scott Reilly (aka coffee2code)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
 files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
@@ -51,21 +57,23 @@ if ( !class_exists('OneClickCloseComments') ) :
 class OneClickCloseComments {
 	var $css_class = 'comment_state';
 	var $field = 'close_comments';
-	var $field_title = 'Close Comments';
 	var $nonce_field = 'update-close_comments';
+	var $textdomain = 'one-click-close-comments';
+	var $field_title = '';
+	var $click_char = '';
 	var $help_text = array();
 
+	/**
+	 * Handles installation tasks, such as ensuring plugin options are instantiated and saved to options table.
+	 *
+	 * @return void
+	 */
 	function OneClickCloseComments() {
 		global $pagenow;
 		if ( !is_admin() || !in_array($pagenow, array('admin-ajax.php', 'edit.php', 'edit-pages.php')) )
 			return;
 
-		$this->help_text = array(
-			0 => __('Comments are closed. Click to open.'),
-			1 => __('Comments are open. Click to close.')
-		);
-		$this->field_title = __($this->field_title);
-
+		add_action('init', array(&$this, 'init'));
 		add_action('admin_head', array(&$this, 'add_css'));
 		add_action('admin_footer', array(&$this, 'add_js'));
 		add_filter('manage_posts_columns', array(&$this, 'add_post_column'));
@@ -75,6 +83,35 @@ class OneClickCloseComments {
 		add_action('wp_ajax_'.$this->field, array(&$this, 'toggle_comment_status'));
 	}
 
+	/**
+	 * Handles actions to be hooked to 'init' action, such as loading text domain and loading plugin config data array.
+	 *
+	 * @return void
+	 */
+	function init() {
+		load_plugin_textdomain( $this->textdomain, false, basename(dirname(__FILE__)) );
+		$this->load_config();
+	}
+
+	/**
+	 * Initializes the plugin's configuration and localizable text variables.
+	 *
+	 * @return void
+	 */
+	function load_config() {
+		$this->help_text = array(
+			0 => __('Comments are closed. Click to open.', $this->textdomain),
+			1 => __('Comments are open. Click to close.', $this->textdomain)
+		);
+		$this->field_title = '';
+		$this->click_char = apply_filters('one-click-close-comments-click-char', '&bull;');
+	}
+
+	/**
+	 * AJAX responder to toggle the comment status for a post (if user if authorized to do so).
+	 *
+	 * @return void
+	 */
 	function toggle_comment_status() {
 		$post_id = isset($_POST['post_id']) ? $_POST['post_id'] : null;
 		check_ajax_referer( $this->field );
@@ -90,62 +127,102 @@ class OneClickCloseComments {
 		die();
 	}
 
+	/**
+	 * Adds a column for the one-click close button to the table of posts in the admin.
+	 *
+	 * @param array $posts_columns Array of post column titles.
+	 * @return array The $posts_columns array with the one-click close comment column's title added.
+	 */
 	function add_post_column( $posts_columns ) {
-		$posts_columns[$this->field] = $this->field_title;
+		// Insert column just before the comments count column.  If that column isn't visible to user, put at end.
+		if ( array_key_exists('comments', $posts_columns) ) {
+			// Damn PHP for not facilitating this.
+			$new_cols = array();
+			foreach ( $posts_columns as $k => $v ) {
+				if ( $k == 'comments' ) { $new_cols[$this->field] = $this->field_title; }
+				$new_cols[$k] = $v;
+			}
+			$posts_columns = $new_cols;
+		} else {
+			$posts_columns[$this->field] = $this->field_title;
+		}
 		return $posts_columns;
 	}
 
+	/**
+	 * Outputs the one-click close button for each post listed in the post listing table in the admin.
+	 *
+	 * @param string $column_name The name of the column.
+	 * @param int $post_id The id of the post being displayed.
+	 * @return void
+	 */
 	function handle_column_data( $column_name, $post_id ) {
 		$post = get_post($post_id);
-		$ajax_url = admin_url() . 'admin-ajax.php';
 		if ( $this->field == $column_name ) {
 			$auth = current_user_can('edit_post', $post_id);
 			$state = ('open' == $post->comment_status ? 1 : 0);
-			if ( $auth ) echo "<a href='$ajax_url' title='" . $this->help_text[$state] . "'>";
-			echo "<span id='" . wp_create_nonce($this->field) . "' class='{$this->css_class}-{$state}'>&bull;</span>";
-			if ( $auth ) echo '</a>';
+
+			if ( $auth ) echo "<span title='" . esc_attr($this->help_text[$state]) . "'>";
+			echo "<span id='" . wp_create_nonce($this->field) . "' class='{$this->css_class}-{$state}'>{$this->click_char}</span>";
+			if ( $auth ) echo '</span>';
 			return;
 		}
 	}
 
+	/**
+	 * Outputs the CSS used by the plugin, within style tags.
+	 *
+	 * @return void (Text is echoed; nothing is returned)
+	 */
 	function add_css() {
 		echo <<<CSS
 		<style type="text/css">
-		.column-{$this->field} { width:1em; display:none; }
+		.column-{$this->field} { width:1em; }
 		td.column-{$this->field} { padding:0; vertical-align:middle; text-align:center;}
 		.{$this->css_class}-1 { font-size:18px; color:#00ff00; }
 		.{$this->css_class}-0 { font-size:18px; color:#ff0000; }
+		.click-span { cursor: pointer; }
 		</style>
 
 CSS;
 	}
 
+	/**
+	 * Outputs the JavaScript used by the plugin, within script tags.
+	 *
+	 * @return void (Text is echoed; nothing is returned)
+	 */
 	function add_js() {
+		$ajax_url = admin_url('admin-ajax.php');
+
 		echo <<<JS
 		<script type="text/javascript">
 		jQuery(document).ready(function($) {
-			$(".column-comments").each(function() {
-				$(this).parents('tr').find('.column-{$this->field}').insertBefore($(this)).show();
+			$(".{$this->field} span").hover(function() {
+				$(this).addClass('click-span');
 			});
 
-			$("th.column-{$this->field}").text('').show();
- 
-			$(".{$this->field} a").click(function() {
+			$(".{$this->field} span").click(function() {
 				var span = $(this).find('span');
 				var current_class = span.attr('class');
+				if ( current_class == undefined )
+					return;
 				var cclass = current_class.split('-');
 				var new_class = cclass[0] + '-';
-				var post_tr = $(this).parents('tr')
+				var post_tr = $(this).parents('tr');
+				var post_id = post_tr.attr('id').substr(5);
 				var help_text = ["{$this->help_text[0]}", "{$this->help_text[1]}"];
-				$.post($(this).attr('href'), {
+				$.post('$ajax_url', {
 						action: "{$this->field}",
 						_ajax_nonce: span.attr('id'),
-						post_id: post_tr.attr('id').substr(5)
+						post_id: post_id
 					}, function(data) {
 						if (data >= 0 && data <= 1) {
 							span.removeClass(current_class);
 							span.addClass(new_class + data);
 							span.parent().attr('title', help_text[data]);
+							// Update hidden field used to configure Quick Edit
+							$('#inline_'+post_id+' div.comment_status').html( (data == '1' ? 'open' : 'closed') );
 						}
 					}, "text"
 				);
--- readme.txt
+++ readme.txt
@@ -1,11 +1,11 @@
 === One Click Close Comments ===
-Contributors: Scott Reilly
+Contributors: coffee2code
 Donate link: http://coffee2code.com/donate
-Tags: comments, close comments, open comments, admin, coffee2code
-Requires at least: 2.6
-Tested up to: 2.8
-Stable tag: 1.1
-Version: 1.1
+Tags: comments, close comments, open comments, admin, comment, discussion, commenting status, coffee2code
+Requires at least: 2.8
+Tested up to: 2.9.1
+Stable tag: 2.0
+Version: 2.0
 
 Conveniently close or open comments for a post or page with one click.
 
@@ -17,18 +17,35 @@ From the admin listing of posts ('Edit Posts') and pages ('Edit Pages'), a user 
 
 This plugin will only function for administrative users in the admin who have JavaScript enabled.
 
+KNOWN ISSUE: After using the 'Quick Edit' for a post, the one click close button for that post no longer works until a page refresh (the button still appears and properly reflect the current status, though).
+
 
 == Installation ==
 
-1. Unzip `one-click-close-comments.zip` inside the `/wp-content/plugins/` directory for your site
+1. Unzip `one-click-close-comments.zip` inside the `/wp-content/plugins/` directory for your site (or install via the built-in WordPress plugin installer)
 1. Activate the plugin through the 'Plugins' admin menu in WordPress
 1. When on the 'Edit Posts' or 'Edit Pages' admin pages, click the indicators to toggle the comment status for a post, as necessary.
 
+
 == Frequently Asked Questions ==
 
-= After activating the plugin, why can't I see the color-coded indicator next to each post on the 'Edit Posts' admin page? =
+= I can see the colored dots indicating current commenting status, but why aren't they clickable? =
 
-This plugin only works for users that have JavaScript enabled.
+The commenting status link/button is only clickable is you have JavaScript enabled.
+
+= What does the color-coding of the dot mean?
+
+Green means the post is currently open for comments; red means the post is not currently open for comments.
+
+= How can I customize the color-coding used for the dot? =
+
+You can customize the colors via CSS.  `.comment-state-1` indicates comments are open.  `.comment-state-0` indicates comments are closed.
+
+= How can I customize the dot used to represent commenting status? =
+
+By default, commenting status is represented using the `&bull;` character.  You can change this by filtering `one-click-close-comments-click-char`.  Here's an example -- added to a theme's functions.php file -- to change it to a solid diamond:
+
+`add_filter( 'one-click-close-comments-click-char', create_function('$a', 'return "&diams";') );`
 
 
 == Screenshots ==
@@ -36,3 +53,29 @@ This plugin only works for users that have JavaScript enabled.
 1. A screenshot of the 'Edit Posts' admin page with the plugin activated.
 
 
+== Changelog ==
+
+= 2.0 =
+* Display commenting status even if JS is disabled
+* Render commenting status as a 'span' instead of an 'a' and use unobtrusive JS to make it clickable
+* Insert column into desired position using PHP instead of JS
+* Fix issue related to disappearance of button for a post after using Quick Edit
+* Fix issue of 'Allow Comments' checkbox in 'Quick Edit' getting out of sync with actual comment status
+* Allow filtering of character used as click link, via 'one-click-close-comments-click-char'
+* Move initialization of config array out of constructor and into new function load_config()
+* Create init() to handle calling load_textdomain() and load_config() (textdomain must be loaded before initializing config)
+* Add support for localization
+* Add PHPDoc documentation
+* Add .pot file
+* Note compatibility with WP 2.9+
+* Drop compatibility with versions of WP older than 2.8
+* Update documentation (descriptions, FAQs, etc) to reflect behavior changes
+* Update copyright date
+
+= 1.1 =
+* Bail out early if not on pertinent admin pages
+* Make use of admin_url() for path to admin section
+* Note WP 2.8 compatibility
+
+= 1.0 =
+* Initial release
